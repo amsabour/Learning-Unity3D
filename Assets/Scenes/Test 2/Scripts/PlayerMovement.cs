5d270cb25de730b11a14caae7c8502de86e599dc
Amir Mojtaba Sabour <amir.mojtaba@yahoo.com>
2020-01-11 18:38:56 +0330

    Added freelook camera using Cinemachine

---
+++ Assets/Scenes/Test 2/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
